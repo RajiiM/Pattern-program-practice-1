6421919ad1f9130e8ca4daa8b4e95b35622dcefe
RajiiM <mrajalakshmiofficial@gmail.com>
2021-11-12 00:22:23 +0530

    FirstCommit

---
+++ Increasing_LeftSide_Triangle_Star.java
@@ -0,0 +1,22 @@
+//Output
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+//
+
+public class Increasing_LeftSide_Triangle_Star {
+
+	public static void main(String[] args) 
+	{ 
+		for (int row = 1;row<=5;row++)
+		{
+			for(int col = 1; col <= row; col++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+}
